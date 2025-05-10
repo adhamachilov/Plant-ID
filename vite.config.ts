c7d1dabb4876d53d9620d6b369ebc66cf747ee76
HP <adhamachilov1007@gmail.com>
2025-05-10 07:03:08 +0500

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/plant-id-1/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
